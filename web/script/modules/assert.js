af1362712f1b1f831817b413191e3bcd11e50917
Shengzhou Zhang <steven.shengzhou@gmail.com>
2013-12-20 21:00:14 +1100

    change assert logic

--- web/script/modules/assert.js
+++ web/script/modules/assert.js
@@ -6,6 +6,10 @@ Modules.Assert = {};
 
 Modules.Assert.assert = function (value1, value2) {
 
+  if (value2 === undefined) {
+    value2 = true;
+  }
+
   if (value1 !== value2) {
     throw {name:'terminate-program', message:'Assert Failed!'};
   }
