16697e1963feb8079bf75442500e983bcd9c0eff
Shengzhou Zhang <steven.shengzhou@gmail.com>
2013-12-21 12:24:43 +1100

    add custom event

--- web/script/modules/fileloader.js
+++ web/script/modules/fileloader.js
@@ -16,33 +16,37 @@ Modules.FileLoader._check = function() {
   Modules.Assert.assert(result);
 };
 
-Modules.FileLoader.load = function(file, callback) {
+Modules.FileLoader.load = function(file, parser) {
 
   var reader, content;
 
   this._check();
 
   Modules.Assert.assert(file instanceof File);
-  Modules.Assert.assert(callback instanceof Function);
+  Modules.Assert.assert(parser instanceof Function);
 
   reader = new FileReader();
 
   reader.onloadend = function(event) {
 
     content = event.target.result;
-
-    callback(content);
+    parser(content);
   };
 
   reader.readAsText(file);
 };
 
+Modules.FileLoader.EVENT_TYPE = "LOAD_FILES";
+
 Modules.FileLoader.LoadHandler = function(event) {
 
-  var files = event.target.files;
+  var i, length, data = event.detail, files = data.files;
 
-  if (files.length > 0) {
+  console.log(event);
+  Modules.Assert.assert(event instanceof Event);
 
-    Modules.FileLoader.load(files[0], function(){});
+  for(i = 0, length = files.length; i < length; i++) {
+
+    Modules.FileLoader.load(files[i], data.parser);
   }
 };
